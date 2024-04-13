353fd1533f452b4a7f6bd5928475233ea464e107
filho.alexandre <filho.alexandre@graduacao.uerj.br>
2024-04-13 18:02:26 -0300

    exemplos e operações em Ocaml

--- T2-Ocaml/Hello_Ocaml/bin/main.ml
+++ T2-Ocaml/Hello_Ocaml/bin/main.ml
@@ -13,7 +13,6 @@ b;;
 let c = (1,"a");;
 c;;
 
-(* Record 
 type carro = {
   modelo: string;
   ano: int;
@@ -22,8 +21,8 @@ type carro = {
 
 let meu_carro = {modelo="Fusca"; ano=1970; odometro=100000};;
 
-*)
-(*
+
+
 type dias_da_semana = 
 | Segunda
 | Terca
@@ -33,8 +32,40 @@ type dias_da_semana =
 | Sabado
 | Domingo;;
 
-Segunda;;
-*)
+Terca;;
 
 
+(*Funções*)
+let funcao parametro = parametro + 1;;
 
+funcao 1;;
+
+funcao 3*2;;  (*          ((funcao 3)*2)        *)
+
+let multiplica a b = a*b;;
+
+multiplica (6*6) 6;;
+
+let rec fatorial n = 
+  if n = 0 then 1
+  else n * fatorial (n-1);;
+
+(+) 2 ((+) 5 ((+) 4 3));;
+
+int_of_float 3.14 + 2;;  (* 5 *)
+float_of_int 2 +. 3.14;;  (* 5.14 *)
+
+let add1 x = x+1;;
+add1 5 |> add1;;
+
+add1 5 |> (+) 6;;  (*       |>  operador pipe *)
+
+multiplica 5 5 |> add1;;
+
+let (>>)  f g x = f(g(x));;  (* criar um novo operador*)
+
+(>>) add1 add1 6;;  
+
+let (+) a b = a * b;;    (*novo operador pode substituir operadores existentes*)
+
+(+) 2 3;; (*6*)
